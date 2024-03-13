b00534d97db78d51e078233fa7a53256388b2f62
Leandro Andrekowicz <103007824+LeandroAndrekowicz@users.noreply.github.com>
2024-03-13 08:32:10 -0300

    Alterando logica

--- BuscaPalavras.sql
+++ BuscaPalavras.sql
@@ -1,50 +1,52 @@
-/*
-select 'Texto linha 1'||E'\n'||'Texto linha 2';
-
-select string_to_array('Texto linha 1'||E'\n'||'Texto linha 2',E'\n');
-*/
-
 create or replace function 
-	contaPalavras(
-		in idTexto integer,
-		in palavra text,
-		out totalLinhas numeric,
-		out totalPalavrasLinhas numeric,
-		out numLinha numeric
-	) returns setof record
+    contaPalavras(
+        in idTexto integer,
+        in palavra text,
+		out numLinha integer,
+        out totalPalavrasLinhas integer,
+		out ocorrencias integer,
+		out totalLinhas numeric
+    ) returns setof record
 AS 
-	$corpo$
+    $corpo$
 DECLARE
-	_linhas text[];
-	_texto text;
-	_linha text;
-	_palavras text[];
-	_palavra text;
-	_contador integer;
-	_contador_geral integer;
-	_i integer;
+    _linhas text[];
+    _texto text;
+    _linha text;
+    _palavras text[];
+    _palavra text;
+    _contador integer := 0;
+    _contador_geral integer := 0;
+    _i integer;
 BEGIN
-	SELECT texto from texto_grande WHERE id = idTexto into _texto;
-	
-	_linhas = string_to_array(_texto,E'\n');
-	
-	for _i in 1 .. array_upper(_linhas, 1) loop
-		_linha = _linhas[_i];
-		_palavras = string_to_array(_linha, ' ');
-		_contador = 0;
-		FOREACH _palavra in ARRAY _palavras
-		loop
-			if palavra = _palavra then
-				_contador =  _contador +1;
-				_contador_geral = _contador_geral + _contador;
-			end if;
-		end loop;
-		totalLinhas = _i;
-	end loop;
-	totalPalavrasLinhas = _contador_geral;
-	return next;
+    SELECT texto FROM texto_grande WHERE id = idTexto INTO _texto;
+    
+    _linhas = string_to_array(_texto, E'\n');
+    totalLinhas := array_length(_linhas, 1);
+
+    for _i in 1 .. array_upper(_linhas, 1) loop
+        _linha = _linhas[_i];
+        _palavras = string_to_array(_linha, ' ');
+        _contador := 0;
+
+        FOREACH _palavra IN ARRAY _palavras LOOP
+            IF palavra = _palavra THEN
+                _contador := _contador + 1;
+                _contador_geral := _contador_geral + _contador;
+            END IF;
+			numLinha := _i;
+        END LOOP;
+		ocorrencias := _contador;
+		totalPalavrasLinhas := _contador_geral;
+
+        RETURN NEXT;
+    END LOOP;
+    
+    RETURN;
 END
-	$corpo$
+$corpo$
 LANGUAGE PLPGSQL;
 
-select * from contaPalavras(2, 'sherek');
+-- Exemplo de uso:
+SELECT * FROM contaPalavras(1, 'George');
+
